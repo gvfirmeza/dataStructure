ac92483d617c1ee0a481e313c4d176a283ae1b8c
Guilherme Firmeza <126484789+gvfirmeza@users.noreply.github.com>
2024-03-22 12:19:27 -0300

    Format simpleCalculate.go

--- acs/ac7/simpleCalculate.go
+++ acs/ac7/simpleCalculate.go
@@ -8,13 +8,13 @@ func main() {
     var a, b int 
     var c float64
 
-	fmt.Scanln(&a, &b, &c)
+    fmt.Scanln(&a, &b, &c)
 	
-	var d, e int 
+    var d, e int 
     var f float64
 
-	fmt.Scanln(&d, &e, &f)
+    fmt.Scanln(&d, &e, &f)
 	
     resultado := float64(b)*c + float64(e)*f
-	fmt.Printf("VALOR A PAGAR: R$ %.2f\n", resultado)
+    fmt.Printf("VALOR A PAGAR: R$ %.2f\n", resultado)
 }
