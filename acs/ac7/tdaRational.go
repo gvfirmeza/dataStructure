017ed76e8db58c71accb327766e0c8d247252c99
gvfirmeza <pmvieira@outlook.com>
2024-03-23 14:54:10 -0300

    ac7-100%

--- acs/ac7/tdaRational.go
+++ acs/ac7/tdaRational.go
@@ -2,33 +2,84 @@ package main
 
 import "fmt"
 
-func mdc(a, b int) int {
-    for b != 0 {
-        a, b = b, a%b
+type Fracao struct {
+    num int
+    den int
+}
+
+func soma(f1 Fracao, f2 Fracao) Fracao {
+    retorno := Fracao{}
+    retorno.num = f1.num*f2.den + f2.num*f1.den
+    retorno.den = f1.den * f2.den
+    return retorno
+}
+
+func subtracao(f1 Fracao, f2 Fracao) Fracao {
+    retorno := Fracao{}
+    retorno.num = f1.num*f2.den - f2.num*f1.den
+    retorno.den = f1.den * f2.den
+    return retorno
+}
+
+func multiplicacao(f1 Fracao, f2 Fracao) Fracao {
+    retorno := Fracao{}
+    retorno.num = f1.num * f2.num
+    retorno.den = f1.den * f2.den
+    return retorno
+}
+
+func divisao(f1 Fracao, f2 Fracao) Fracao {
+    retorno := Fracao{}
+    retorno.num = f1.num * f2.den
+    retorno.den = f2.num * f1.den
+    return retorno
+}
+
+func MDC(a int, b int) int {
+    if b == 0 {
+        return a
     }
-    return a
+    return MDC(b, a%b)
+}
+
+func irredutivel(f Fracao) Fracao {
+    mdc := 0
+    if f.num < 0 {
+        mdc = MDC(-f.num, f.den)
+    } else {
+        mdc = MDC(f.num, f.den)
+    }
+    f.num /= mdc
+    f.den /= mdc
+    return f
 }
 
 func main() {
-    var n int
-    fmt.Scanf("%d", &n)
-    for i := 0; i < n; i++ {
-        var x, y int
-        var op string
-        fmt.Scanf("%d %s %d", &x, &op, &y)
-        var res int
-        switch op {
-        case "+":
-            res = x*y + y*x
-        case "-":
-            res = x*y - y*x
-        case "*":
-            res = x * y
-        case "/":
-            res = x * y
+    var N int
+    var operador byte
+    var f1 Fracao
+    var f2 Fracao
+    var resultado Fracao
+    fmt.Scanf("%d\n", &N)
+    for i := 0; i < N; i++ {
+        fmt.Scanf("%d / %d %c %d / %d\n", &f1.num, &f1.den, &operador, &f2.num, &f2.den)
+        switch operador {
+        case '+':
+            resultado = soma(f1, f2)
+            break
+        case '-':
+            resultado = subtracao(f1, f2)
+            break
+        case '*':
+            resultado = multiplicacao(f1, f2)
+            break
+        case '/':
+            resultado = divisao(f1, f2)
+            break
         }
-        num, den := res, x*y
-        gcd := mdc(num, den)
-        fmt.Printf("%d/%d\n", num/gcd, den/gcd)
+        fmt.Printf("%d/%d = ", resultado.num, resultado.den)
+        resultado = irredutivel(resultado)
+        fmt.Printf("%d/%d\n", resultado.num, resultado.den)
     }
 }
+
